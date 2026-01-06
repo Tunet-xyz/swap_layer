ebddd484d02e27c6051af44c7d4bd767c2c2f6f6
Alex <bezman.alexc@gmail.com>
2026-01-06 02:59:28 +0000

    Remove localization module and related files
    Deleted the entire localization module, including country configuration and geolocation service files. This change removes country-specific jurisdiction and language mapping, as well as Azure Maps-based geolocation functionality.

--- src/swap_layer/localization/__init__.py
+++
@@ -1,8 +0,0 @@
-from .countries import COUNTRY_CONFIG, get_country_config
-from .geolocation import GeolocationService
-
-__all__ = [
-    'GeolocationService',
-    'COUNTRY_CONFIG',
-    'get_country_config',
-]
